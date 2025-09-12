7224e0d6a5505f4fd6c767389e5035fd5eff3e88
AntoineSPR <antoine.simper@gmail.com>
2025-09-12 20:29:19 +0200

    Add Unit Test project

---
+++ Procrastinator/Dockerfile
@@ -0,0 +1,31 @@
+ # This stage is used when running from VS in fast mode (Default for Debug configuration)
+FROM mcr.microsoft.com/dotnet/aspnet:8.0-alpine AS base
+# Install ICU libraries
+RUN apk add --no-cache icu-libs
+# Disable globalization invariant mode
+ENV DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=false
+USER $APP_UID
+WORKDIR /app
+EXPOSE 8080
+
+
+# This stage is used to build the service project
+FROM mcr.microsoft.com/dotnet/sdk:8.0-alpine AS build
+ARG BUILD_CONFIGURATION=Release
+WORKDIR /src
+COPY ["Procrastinator.csproj", "."]
+RUN dotnet restore "./Procrastinator.csproj"
+COPY . .
+WORKDIR "/src/."
+RUN dotnet build "./Procrastinator.csproj" -c $BUILD_CONFIGURATION -o /app/build
+
+# This stage is used to publish the service project to be copied to the final stage
+FROM build AS publish
+ARG BUILD_CONFIGURATION=Release
+RUN dotnet publish "./Procrastinator.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
+
+# This stage is used in production or when running from VS in regular mode (Default when not using the Debug configuration)
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "Procrastinator.dll"]
---
+++ TestsUnitaires/QuestServiceTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Procrastinator.Context;
+using Procrastinator.Services;
+
+namespace TestsUnitaires
+{
+    public class QuestServiceTests
+    {
+        private readonly DataContext _context;
+        private readonly QuestService _questService;
+
+        public QuestServiceTests(
+            )
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new DataContext(options);
+
+            _questService = new QuestService(_context);
+        }
+
+        public async Task GetQuestByIdAsync_WithValidId_ReturnsQuest()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            var quest = new Procrastinator.Models.Quest
+            {
+                Id = Guid.NewGuid(),
+                UserId = userId,
+                Title = "Test Quest",
+                Description = "This is a test quest",
+                IsAssigned = false,
+                IsDone = false
+            };
+            await _context.Quests.AddAsync(quest);
+            await _context.SaveChangesAsync();
+            // Act
+            var result = await _questService.GetQuestByIdAsync(quest.Id, userId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Single(_context.Quests);
+            Assert.Equal(quest.Id, result.Id);
+            Assert.Equal(userId, result.UserId);
+        }
+    }
+}
