f32f949fac4111b00e5152788d28f28fedfdbaec
AntoineSPR <antoine.simper@gmail.com>
2025-09-17 19:47:29 +0200

    Add Charge Tests

---
+++ Procrastinator/Controllers/FixturesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Procrastinator.Services;
+
+namespace Procrastinator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FixturesController : ControllerBase
+    {
+        private readonly FixturesService fixturesService;
+
+        public FixturesController(FixturesService fixturesService)
+        {
+            this.fixturesService = fixturesService;
+        }
+
+        [HttpPost("create-users/{number}")]
+        public IActionResult CreateUsers(int number)
+        {
+            fixturesService.CreateUsers(number);
+            return Ok($"Created {number} users.");
+        }
+
+        [HttpPost("create-quests/{number}")]
+        public async Task<IActionResult> CreateQuests(int number)
+        {
+            await fixturesService.CreateQuests(number);
+            return Ok($"Created {number} quests for each user.");
+        }
+    }
+}
